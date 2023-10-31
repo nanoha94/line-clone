f3ee1ff00ab5c0b0d4668a8427cdf0bb448d8dce
nanoha <nanoha.creator@gmail.com>
2023-10-31 14:18:01 +0000

    サインイン画面のスタイル調整

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -2,14 +2,27 @@ import React from "react";
 import { Button } from "@mui/material";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../firebase";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  width: 100%;
+  padding: 20px;
+  display: flex;
+`;
 
 const SignIn = () => {
- const signInWithGoogle = () => {
+  const signInWithGoogle = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider);
   };
 
-  return <Button onClick={signInWithGoogle}>グーグルでログインする</Button>;
+  return (
+    <Container>
+      <Button variant="outlined" onClick={signInWithGoogle}>
+        Googleでログインする
+      </Button>
+    </Container>
+  );
 };
 
 export default SignIn;
