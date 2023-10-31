519536dedec8508088a17010017d69b511254c0a
nanoha <nanoha.creator@gmail.com>
2023-10-31 05:41:34 +0000

    firebase設定

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA8lBeebvR22ja_IUNflhlv0mPQVoem2f4",
+  authDomain: "line-clone-f48c7.firebaseapp.com",
+  projectId: "line-clone-f48c7",
+  storageBucket: "line-clone-f48c7.appspot.com",
+  messagingSenderId: "109212675637",
+  appId: "1:109212675637:web:b9e22609bcb1e2ff0fcb7c"
+};
