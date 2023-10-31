baf2eeb1841e4e73204d07a78ade5277db9a04e6
nanoha <nanoha.creator@gmail.com>
2023-10-31 08:12:53 +0000

    データベースからのメッセージ受け取り実装

--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -1,14 +1,61 @@
-import React from 'react'
-import { Button } from "@mui/material";
-import { signOut } from '../firebase';
-import SignOut from './SignOut';
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
+import {
+  Timestamp,
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  limit,
+} from "firebase/firestore";
+import SignOut from "./SignOut";
+
+type Message = {
+  text: string;
+  created_at: Timestamp;
+};
 
 const Line = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const get_messages = async () => {
+      const q = query(
+        collection(db, "messages"),
+        orderBy("created_at"),
+        limit(50)
+      );
+      const snapshot = await getDocs(q);
+      const dbMessages: Message[] = [];
+      snapshot.forEach((doc) => {
+        dbMessages.push({
+          text: doc.data().text,
+          created_at: doc.data().created_at,
+        });
+      });
+      setMessages(dbMessages);
+    };
+
+    get_messages();
+  }, []);
+
+  useEffect(() => {
+    console.log(messages);
+  }, [messages]);
+
   return (
     <>
-    <SignOut />
+      <SignOut />
+      <div>
+        {messages.map(
+          (message) => (
+            // TODO: keyはあとで見直し
+            <p key={message.text}>{message.text}</p>
+          ) 
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Line
+export default Line;
